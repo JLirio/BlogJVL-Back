ce2a2cf57cce14a402f9e403b8a016d6704a3d8d
Jao <joaovictorjgar@gmail.com>
2021-08-02 10:38:18 -0300

    Deploy

---
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public List <Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	
+	public Optional <Usuario> findByUsuario(String usuario);
+	
+	public Usuario findByNome(String nome);
+	
+}
+
